6ed0bd7b90528a924ce5ee1ec25dbe83bc47d05d
Minh Son <minhson892002@gmail.com>
2022-04-09 15:26:26 +0700

    push code

---
+++ app.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  $(".testimonial__list").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    infinite: true,
+    arrows: false,
+    dots: true,
+  });
+});
